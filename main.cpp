1e0ac38b109ad94cd4365a53fb239b9cb3b0dc26
procedural <iamvfx@gmail.com>
2022-05-24 12:04:14 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <fstream>
+#include <cstdint>
+#include <cstdio>
+#include <new>
+
+void generateHeader(size_t dataBytesCount, unsigned char * data) {
+  printf(
+    "#pragma once\n"
+    "\n"
+    "size_t resourceGetBytesCount() {\n"
+    "  return %zu;\n"
+    "}\n"
+    "\n"
+    "void resourceWriteToMemory(void * memory) {\n"
+    "  unsigned char * m = (unsigned char *)memory;\n",
+    dataBytesCount
+  );
+  for (size_t i = 0; i < dataBytesCount; i += 1) {
+    printf("  m[%zu] = %d;\n", i, data[i]);
+  }
+  printf("}\n");
+}
+
+int main(int argsCount, const char ** args) {
+  if (argsCount < 2) {
+    printf("Usage example: headergen.exe C:/path/to/some.file");
+    return 0;
+  }
+
+  const char * filepath = args[1];
+
+  std::ifstream file(filepath, std::ifstream::binary);
+  if (file.fail() == false) {
+    std::filebuf * fileFb = file.rdbuf();
+    if (fileFb != 0) {
+      size_t fileDataBytesCount = fileFb->pubseekoff(0, file.end, file.in);
+      fileFb->pubseekpos(0, file.in);
+      if (fileDataBytesCount > 0) {
+        char * fileData = new char[fileDataBytesCount];
+        fileFb->sgetn(fileData, fileDataBytesCount);
+
+        generateHeader(fileDataBytesCount, (unsigned char *)fileData);
+
+        delete[] fileData;
+      }
+    }
+  }
+  file.close();
+
+  return 0;
+}
